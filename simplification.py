54c45f97559f342fb67128e8259aaabf1973729d
Gregor Kovač <79193306+gregorkovac@users.noreply.github.com>
2023-12-23 11:15:48 +0100

    Link condition lemma

--- simplification.py
+++ simplification.py
@@ -1,5 +1,50 @@
 import matplotlib.pyplot as plt
 
+'''
+vertex_link(T, v) returns the link of vertex v = (x, y) in the triangulation T.
+'''
+def vertex_link(T, v):
+    link = []
+
+    for t in T:
+        if v in t:
+            other_vertices = [x for x in t if x != v]
+
+            link.append(other_vertices[0])
+            link.append(other_vertices[1])
+            link.append((other_vertices[0], other_vertices[1]))
+
+    return link
+
+'''
+edge_link(T, e) returns the link of edge e = [(x1, y1), (x2, y2)] in the triangulation T.
+'''
+def edge_link(T, e):
+    link = []
+
+    for t in T:
+        if e[0] in t and e[1] in t:
+            for v in t:
+                if v != e[0] and v != e[1]:
+                    link.append(v)
+                    break
+
+    return link
+
+'''
+is_safe(T, e) returns True if it is safe to contract edge e = [(x1, y1), (x2, y2)] in the 
+triangulation T using the link condition lemma.
+'''
+def is_safe(T, e):
+    link_ab = edge_link(T, e)
+    link_a = vertex_link(T, e[0])
+    link_b = vertex_link(T, e[1])
+
+    return set(link_ab) == set(link_a).intersection(set(link_b))
+
+'''
+contract_edge(T, e) contracts edge e = [(x1, y1), (x2, y2)] in the triangulation T.
+'''
 def contract_edge(T, e):
     middle_point = [(e[0][0] + e[1][0]) / 2, (e[0][1] + e[1][1]) / 2]
 
@@ -28,6 +73,8 @@ def main():
         plt.plot([t[0][0], t[1][0]], [t[0][1], t[1][1]], 'k--')
         plt.plot([t[1][0], t[2][0]], [t[1][1], t[2][1]], 'k--')
         plt.plot([t[2][0], t[0][0]], [t[2][1], t[0][1]], 'k--')
+
+    print(is_safe(T, [(0, 2), (2, 2)]))
 
     contract_edge(T, [(0, 2), (2, 2)])
 
